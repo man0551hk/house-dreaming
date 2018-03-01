f7caacd32d23a9310d59d5737d3402f2fcd9b128
Jerry Wong <jerry.wong@iproperty.com>
2018-03-01 17:47:50 +0800

    update

--- Search.aspx.cs
+++ Search.aspx.cs
@@ -48,5 +48,71 @@ public partial class Search : Page_Control
     protected void searchBtn_Click(object sender, EventArgs e)
     {
         testMsg.Text = DateTime.Now.ToString();
+
+        string keyword = keywordText.Text;
+        string districtID = districtDDL.SelectedValue == "0" ? "" : districtDDL.SelectedValue;
+        string room = roomDDL.SelectedValue == "0" ? "" : roomDDL.SelectedValue;
+        string netsize = "between 0 and 999999999";
+        switch (netsizeDDL.SelectedValue)
+        {
+            case "1": netsize = "between 0 and 200";
+                break;
+            case "2": netsize = "between 200 and 400";
+                break;
+            case "3": netsize = "between 400 and 600";
+                break;
+            case "4": netsize = "between 600 and 800";
+                break;
+            case "5": netsize = "between 800 and 1000";
+                break;
+            case "6": netsize = ">= 1000";
+                break;  
+        }
+        string listingType = listingTypeDDL.SelectedValue == "3" ? "" : listingTypeDDL.SelectedValue;
+        string saleprice = "";
+        switch (salepriceDDL.SelectedValue)
+        {
+            case "1": saleprice = "between 0 and 2000000";
+                break;
+            case "2": saleprice = "between 2000000 and 4000000";
+                break;
+            case "3": saleprice = "between 4000000 and 6000000";
+                break;
+            case "4": saleprice = "between 6000000 and 8000000";
+                break;
+            case "5": saleprice = "between 8000000 and 10000000";
+                break;
+            case "6": netsize = ">= 100000009";
+                break;
+        }
+        string rentprice = "";
+        switch (rentPriceDDL.SelectedValue)
+        {
+            case "1": rentprice = "between 0 and 10000";
+                break;
+            case "2": rentprice = "between 10000 and 20000";
+                break;
+            case "3": rentprice = "between 20000 and 30000";
+                break;
+            case "4": rentprice = ">= 30000";
+                break;
+        }
+        SearchListing(keyword, districtID, room, netsize, listingType, saleprice, rentprice);
+    }
+
+    private void SearchListing(string keyword, string districtID, string room, string netsize, string listingType, string saleprice, string rentprice)
+    {
+        testMsg.Text = keyword + " " + districtID + " " + room + " " + netsize + " " + listingType + " " + saleprice + " " + rentprice;
+        try
+        {
+            cn.Open();
+            string sql = "";
+        }
+        catch (Exception ex)
+        { }
+        finally
+        {
+            cn.Close();
+        }
     }
 }
